8e2718657499375d53f0a121d652d675ee2ff6a4
ManikanthDaru <manikanthdaru82@gmail.com>
2025-03-04 13:39:32 +0530

    Hosting backend

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,7 @@ app.use("/api/products", productRoutes);
 
 // console.log(process.env.MONGO_URI);
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-}
+
 app.listen(PORT, () => {
   connectDB();
   console.log("Server started on http://localhost:5000");
